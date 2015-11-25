2e27bdf1029f0884203bdd2637ead24a0ef59ef3
Josery <linuxjosery@gmail.com>
2015-11-25 22:28:33 +0800

    Add mult_ssh.sh

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # shell
 some of my used shell scripts
+
+### How to use
+1.mult_ssh.sh <br>
+establish a trust relationship of SSH (admin -> server...)
+```shell 
+ssh-keygen -t rsa -b 2048 -N '' -f ~/.ssh/ssh-id_rsa
+ssh-copy-id -i ~/.ssh/ssh-id_rsa.pub 192.168.1.11
+```
---
+++ mult_ssh.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Used to:   Multiple SSH run commands
+# You can change the 'COMMAND'
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 {server_file}"
+	exit 1
+fi
+
+BASE_DIR="`dirname $0`/.adm"
+KEY_FILE='/root/.ssh/ssh-id_rsa'
+SERVER_FILE=$1
+COMMAND="yum -y install httpd"
+USER='root'
+
+# Ensure BASE_DIR exist
+test -d ${BASE_DIR} || mkdir -p ${BASE_DIR}
+
+# main body
+for HOST in `cat ${SERVER_FILE}`; do 
+	echo $HOST:
+	ssh -n -i ${KEY_FILE} ${USER}@${HOST} ${COMMAND} 
+	echo
+done
+
+
